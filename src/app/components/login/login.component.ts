746623d5821a71a93f094a7c106419d62b01080c
Jul Bar <barankovauv@gmail.com>
2022-09-27 11:43:46 +0300

    login service + logout

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,15 +1,50 @@
+import { Router } from '@angular/router';
+import { AuthService } from '../../services/auth.service';
+import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { AngularFireAuth } from '@angular/fire/compat/auth';
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-login',
-  templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+    selector: 'app-login',
+    templateUrl: './login.component.html',
+    styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+    loginForm: FormGroup;
+    firebaseErrorMessage: string;
 
-  ngOnInit(): void {
-  }
+    constructor(
+      private authService: AuthService,
+      private router: Router,
+      private afAuth: AngularFireAuth)
+      {
+        this.loginForm = new FormGroup({
+            'email': new FormControl('', [Validators.required, Validators.email]),
+            'password': new FormControl('', Validators.required)
+        });
+        this.firebaseErrorMessage = '';
+    }
 
+    ngOnInit(): void {
+        if (this.authService.userLoggedIn) {  // if the user's logged in, navigate them to the dashboard (NOTE: don't use afAuth.currentUser -- it's never null)
+            this.router.navigate(['/dashboard']);
+        }
+    }
+
+    loginUser() {
+        if (this.loginForm.invalid)
+            return;
+
+        this.authService.loginUser(this.loginForm.value.email, this.loginForm.value.password).then((result) => {
+            if (result == null) {// null is success, false means there was an error
+                console.log('logging in...');
+            }
+            else if (result.isValid == false) {
+                console.log('login error', result);
+                this.firebaseErrorMessage = result.message;
+            }
+        });
+    }
 }
+
